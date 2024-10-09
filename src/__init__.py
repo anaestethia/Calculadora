14e8885db5e00e77d5d96c35c2c5baaddb82f210
Franco <francobocanegra667@gmail.com>
2024-10-09 19:08:50 -0300

    edit de init

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,1 @@
-# Este archivo está vacío intencionalmente.
-# Su presencia indica que este directorio debe ser tratado como un paquete de Python.
-
 from .calculadora import calculadora, operaciones, mostrar_historial
